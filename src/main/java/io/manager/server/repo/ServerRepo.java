fd436460336f494767203cefe86064e85cdce3d2
Radu <radu.constantinescu96@protonmail.com>
2022-01-30 15:04:03 +0200

    First Stage

---
+++ src/main/java/io/manager/server/repo/ServerRepo.java
@@ -0,0 +1,8 @@
+package io.manager.server.repo;
+
+import io.manager.server.model.Server;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServerRepo extends JpaRepository<Server, Long> {
+    Server findByIpAddress(String ipAddress);
+}
